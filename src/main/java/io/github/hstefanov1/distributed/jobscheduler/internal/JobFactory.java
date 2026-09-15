ce3bfede9d488e65f8062a0ea9d71d272ccd702e
hstefanov1 <269922586+hstefanov1@users.noreply.github.com>
2026-09-15 23:42:20 +0100

    refactor: consolidate job validator in job factory

--- src/main/java/io/github/hstefanov1/distributed/jobscheduler/internal/JobFactory.java
+++ src/main/java/io/github/hstefanov1/distributed/jobscheduler/internal/JobFactory.java
@@ -2,21 +2,24 @@ package io.github.hstefanov1.distributed.jobscheduler.internal;
 
 import io.github.hstefanov1.distributed.jobscheduler.api.JobName;
 import io.github.hstefanov1.distributed.jobscheduler.api.JobProcessor;
+import io.quarkus.runtime.Startup;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Instance;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+@Slf4j
 @ApplicationScoped
 class JobFactory {
 
     private final Map<JobName, JobProcessor> processors;
 
     JobFactory(Instance<JobProcessor> instances) {
-        this.processors = instances.stream()
-                .collect(Collectors.toMap(JobProcessor::name, Function.identity()));
+        this.processors = instances.stream().collect(Collectors.toMap(JobProcessor::name, Function.identity()));
     }
 
     JobProcessor get(JobName name) {
@@ -26,8 +29,28 @@ class JobFactory {
             // 1) your processor implements the interface JobProcessor
             // 2) your processor#name() returns the expected JobName
             // 3) your processor is public and application-scoped
-            throw new IllegalStateException("No JobProcessor registered for job [%s]".formatted(name));
+            throw new IllegalStateException("No job processor registered for job name [%s]".formatted(name));
         }
         return processor;
+    }
+
+    @Startup
+    void onStart() {
+        log.info("Validating job processors");
+
+        // retrieve all jobs
+        List<JobConfig> jobs;
+        try {
+            jobs = JobConfig.<JobConfig>findAll().list();
+        } catch (Exception e) {
+            throw new IllegalStateException("A job name may refer to a non-existent enum value", e);
+        }
+
+        // validate processors for each job
+        for (JobConfig job : jobs) {
+            get(job.jobName); // throws exception if processor not found
+        }
+
+        log.info("Validated job processors");
     }
 }
--- src/test/java/io/github/hstefanov1/distributed/jobscheduler/internal/JobFactoryTest.java
+++ src/test/java/io/github/hstefanov1/distributed/jobscheduler/internal/JobFactoryTest.java
@@ -2,45 +2,86 @@ package io.github.hstefanov1.distributed.jobscheduler.internal;
 
 import io.github.hstefanov1.distributed.jobscheduler.api.JobName;
 import io.github.hstefanov1.distributed.jobscheduler.api.JobProcessor;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import jakarta.enterprise.inject.Instance;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 @SuppressWarnings("unchecked")
 @ExtendWith(MockitoExtension.class)
 class JobFactoryTest {
+
+    @Mock
+    private JobProcessor processorMock;
 
     @Test
     void get_WhenProcessorIsNotRegistered_ThenAnExceptionIsThrown() {
         Instance<JobProcessor> instances = mock(Instance.class);
         doReturn(Stream.empty()).when(instances).stream();
 
-        JobFactory factory = new JobFactory(instances);
+        JobFactory instance = new JobFactory(instances);
 
         IllegalStateException e = assertThrows(IllegalStateException.class,
-                () -> factory.get(JobName.DEACTIVATE_EXPIRED));
-        assertEquals("No JobProcessor registered for job [DEACTIVATE_EXPIRED]", e.getMessage());
+                () -> instance.get(JobName.DEACTIVATE_EXPIRED));
+        assertEquals("No job processor registered for job name [DEACTIVATE_EXPIRED]", e.getMessage());
     }
 
     @Test
     void get_WhenProcessorIsRegistered_ThenIsReturned() {
-        JobProcessor processorMock = mock(JobProcessor.class);
+        JobFactory instance = createInstance();
+        JobProcessor result = instance.get(JobName.DEACTIVATE_EXPIRED);
+        assertEquals(processorMock, result);
+    }
+
+    @Test
+    void onStart_WhenExceptionOnFindAllJobs_ThenAnExceptionIsThrown() {
+        JobFactory instance = createInstance();
+        try (MockedStatic<PanacheEntityBase> mock = Mockito.mockStatic(PanacheEntityBase.class)) {
+            mock.when(JobConfig::findAll).thenThrow(RuntimeException.class);
+
+            IllegalStateException e = assertThrows(IllegalStateException.class, instance::onStart);
+            assertEquals("A job name may refer to a non-existent enum value", e.getMessage());
+
+            mock.verify(PanacheEntityBase::findAll);
+        }
+    }
+
+    @Test
+    void onStart_WhenFindAllJobsIsCalled_ThenGetIsCalled() {
+        JobFactory instanceSpy = spy(createInstance());
+        doReturn(null).when(instanceSpy).get(any());
+
+        try (MockedStatic<PanacheEntityBase> mock = Mockito.mockStatic(PanacheEntityBase.class)) {
+            PanacheQuery<JobConfig> query = mock(PanacheQuery.class);
+            doReturn(List.of(mock(JobConfig.class))).when(query).list();
+            mock.when(JobConfig::findAll).thenReturn(query);
+
+            instanceSpy.onStart();
+
+            mock.verify(PanacheEntityBase::findAll);
+        }
+
+        verify(instanceSpy, times(1)).get(any());
+    }
+
+    private JobFactory createInstance() {
         doReturn(JobName.DEACTIVATE_EXPIRED).when(processorMock).name();
 
         Instance<JobProcessor> instances = mock(Instance.class);
         doReturn(Stream.of(processorMock)).when(instances).stream();
 
-        JobFactory factory = new JobFactory(instances);
-
-        JobProcessor result = factory.get(JobName.DEACTIVATE_EXPIRED);
-        assertEquals(processorMock, result);
+        return new JobFactory(instances);
     }
 }
